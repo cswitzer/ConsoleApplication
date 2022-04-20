ae78619eccf144ed2b0d4dfcc8fc67ca4c4cb635
Chase <chasehillig@yahoo.com>
2022-04-19 22:44:04 -0600

    added first 5 steps

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -10,6 +10,32 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
+            string name;
+            string location;
+            
+            // step 2
+            Console.Write("Please enter your name: ");
+            name = Console.ReadLine();
+            Console.Write("Please enter your location: ");
+            location = Console.ReadLine();
+
+            // step 3
+            Console.WriteLine("Your name is " + name);
+            Console.WriteLine("You are from " + location);
+
+            // step 4
+            DateTime currentDate = DateTime.Today;
+            Console.WriteLine("Today's date is " + currentDate.ToString("d"));
+
+            // step 5
+            DateTime christmasDate = new DateTime(2022, 12, 25);
+            int numDaysTilChristmas = christmasDate.Subtract(currentDate).Days;
+            Console.WriteLine("There are " + numDaysTilChristmas + " 'til Christmas!");
+
+            // step 6
+
+
+            Console.ReadKey();
         }
     }
 }
